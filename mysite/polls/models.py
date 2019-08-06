1ea5b28e6e887f239d1b1f60cb8f24e0d4eb2e73
Alex shevelo <oshevelo@softserveinc.com>
2019-08-06 20:43:24 +0300

    nested ser

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,7 +6,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=15, default='1')
     description = models.TextField()
     pub_date = models.DateTimeField('date published')
-    
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        null=True,
+        blank=True,
+    );
+
     def __str__(self):
         return self.question_text
 
--- mysite/polls/serializers.py
+++ mysite/polls/serializers.py
@@ -1,9 +1,23 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from polls.models import Question
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'is_active', 'last_login')
+
+
 class QuestionSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    email = serializers.SerializerMethodField(read_only=True)
+
+    def get_email(self, obj):
+        return obj.author.email if obj.author else ''
+
     class Meta:
         model = Question
-        fields = ('id', 'question_text', 'description', 'pub_date')
+        fields = ('id', 'email', 'question_text', 'description', 'pub_date', 'author')
 
