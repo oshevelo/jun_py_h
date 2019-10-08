9d23fc69fe94d35eecd4eec90149d6d9ba76b4e1
Dmytro Aslyaev <aslyaev@gmail.com>
2019-10-08 19:37:30 +0300

    car and feature models

--- car/products/admin.py
+++ car/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from products.models import Car, Feature
+
+admin.site.register(Car)
+admin.site.register(Feature)
--- car/products/models.py
+++ car/products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    """Машина, которую выбирает покупатель"""
+    make = models.CharField(max_length=30)
+    model = models.CharField(max_length=40)
+    vin = models.CharField(max_length=17)
+    year_of_prod = models.IntegerField(max_length=4)
+    base_usd_price = models.IntegerField()
+
+    def __str__(self):
+        return self.make + " " + self.model + ", " + str(self.year_of_prod)
+
+
+class Feature(models.Model):
+    """Элемент комплектации, который добавляет покупатель к базовой комплектации автомобиля"""
+    car = models.ForeignKey(Car,
+          on_delete = models.CASCADE,
+          null = True,
+          blank = True,
+          )
+    feature_name = models.CharField(max_length=30)
+    feature_usd_price = models.IntegerField()
+
+    def __str__(self):
+        return self.feature_name
+        
