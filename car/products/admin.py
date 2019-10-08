47c68bf2e66e18378435f71911d41a075a61cf43
Dmytro Aslyaev <aslyaev@gmail.com>
2019-10-08 21:06:02 +0300

    admin site superuser plus register

--- car/products/admin.py
+++ car/products/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from products.models import Car, Feature
 
-admin.site.register(Car)
+class FeatureInline(admin.StackedInline):
+    model = Feature
+    extra = 3
+
+
+class CarAdmin(admin.ModelAdmin):
+    fieldsets = [(None,               {'fields': ['make', 'model', 'vin', 'year_of_prod',        'base_usd_price']})]
+    inlines = [FeatureInline]
+
+
+
+admin.site.register(Car, CarAdmin)
 admin.site.register(Feature)
