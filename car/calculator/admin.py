d0f59b29a48704facb675462c2eee6bbf438bc89
kudzan69 <kudzan69@gmail.com>
2019-10-08 20:41:46 +0300

    prototype cureency exchenger

--- car/calculator/admin.py
+++ car/calculator/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from calculator.models import ExchangeRate
 
-# Register your models here.
+
+admin.site.register(ExchangeRate)
--- car/calculator/models.py
+++ car/calculator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class ExchangeRate (models.Model):
+
+    currency_code = models.CharField(max_length=20)
+
+    date = models.DateField(max_length=20, help_text="Enter field documentation")
+
+    exchange_rate = models.FloatField(max_length=20)
+
+    
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.field_name
+	
