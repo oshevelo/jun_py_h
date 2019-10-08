d6e9b3ae49d6b682e395da4be6e7e3f4166d7827
Dmytro Aslyaev <aslyaev@gmail.com>
2019-10-08 20:23:56 +0300

    migrations and model docstring translate

--- car/products/models.py
+++ car/products/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Car(models.Model):
-    """Машина, которую выбирает покупатель"""
+    """Car being chosen by customer"""
     make = models.CharField(max_length=30)
     model = models.CharField(max_length=40)
     vin = models.CharField(max_length=17)
-    year_of_prod = models.IntegerField(max_length=4)
+    year_of_prod = models.IntegerField()
     base_usd_price = models.IntegerField()
 
     def __str__(self):
@@ -15,7 +15,7 @@ class Car(models.Model):
 
 
 class Feature(models.Model):
-    """Элемент комплектации, который добавляет покупатель к базовой комплектации автомобиля"""
+    """Equipment item being added to car base equipment by customer"""
     car = models.ForeignKey(Car,
           on_delete = models.CASCADE,
           null = True,
