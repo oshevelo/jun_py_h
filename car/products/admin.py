ae4af7cf69db3329e05b26be96b5e1dbf8c07e03
Dmytro Aslyaev <aslyaev@gmail.com>
2019-10-10 16:48:44 +0300

    search and list display

--- car/products/admin.py
+++ car/products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from products.models import Car, Feature
 
-class FeatureInline(admin.StackedInline):
+class FeatureInline(admin.TabularInline):
     model = Feature
     extra = 3
 
@@ -9,7 +9,8 @@ class FeatureInline(admin.StackedInline):
 class CarAdmin(admin.ModelAdmin):
     fieldsets = [(None,               {'fields': ['make', 'model', 'vin', 'year_of_prod',        'base_usd_price']})]
     inlines = [FeatureInline]
-
+    list_display = ['make', 'model', 'vin', 'year_of_prod',        'base_usd_price']
+    search_fields = ['make', 'model', 'vin', 'year_of_prod',        'base_usd_price']
 
 
 admin.site.register(Car, CarAdmin)
